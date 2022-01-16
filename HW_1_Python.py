e9c1bd6cae907e1f3bb48ae6d1d636430eabfb1d
unknown <tatiana.namestnykova@gmail.com>
2022-01-16 17:57:19 +0200

    add HW_1_Python.py

---
+++ HW_1_Python.py
@@ -0,0 +1,50 @@
+name = 'Egor'
+age = 12
+height = 162.5
+byt = bytes(100)
+list = ['Egor', 12, 162.5]
+tuple = ('Egor', 12, 23.11)
+set = {1, 2, 2, 'data', 'data'}
+froz = frozenset({"hello", "Python"})
+dict = {"one": "This is one"}
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(byt))
+print(type(list))
+print(type(tuple))
+print(type(set))
+print(type(froz))
+print(type(dict))
+country = "Ukraine"
+city = "Kharkov"
+motherland = country+city
+print(motherland)
+rose = 'flower'
+quantity = '3'
+bouquet = (rose , quantity)
+print(bouquet)
+flowers = ('rose' + quantity)
+print(flowers)
+q = 100
+w = 5
+e = q/w
+print(e)
+r = q * w
+print(r)
+t = 25
+s = 6
+g = t/s
+print(int(g))
+cat_1 = 30
+cat_2 = 7
+res = cat_1 // cat_2
+print(res)
+n = 17
+k = 5
+v = n % k
+print(v)
+z = (7 + 12) ** 3 + 7 * 4 - 44 / 2 ** 4
+print(z)
+
+
