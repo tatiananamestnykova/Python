3e624ce56fc9539a99465a05e223daef9d60f06a
unknown <tatiana.namestnykova@gmail.com>
2022-01-17 16:43:24 +0200

    add  HW_1_Python

--- HW_1_Python.py
+++ HW_1_Python.py
@@ -9,7 +9,6 @@ tuple = ('Egor',12, 23.11) #6.Создать переменную типа Tuple
 set = {1, 2, 2, 'data', 'data'} #7.Создать переменную типа Set
 froz = frozenset({"hello", "Python"}) #8.Создать переменную типа Frozen set
 dict = {"one": "This is one"} #9. Создать переменную типа Dict
-
 print('name=', name, type(name))
 print('age=', age, type(age))
 print('height=', height, type(height))
@@ -35,18 +34,16 @@ print(name, age)
 d = a/b
 print(d)
 
-print(name+age)
+print('name'+'age')
 
 n = a*b
 print(n)
 
-int(12.4)
-round(12.4)
-math.floor(13.8)
-math.ceil(13.8)
+print(int(d))
+print(round(d))
 
 e = a % b
 print(e)
 
 z = (7 + 12) ** 3 + 7 * 4 - 44 / 2 ** 4
-print(z) #6884.25
+print(z)
