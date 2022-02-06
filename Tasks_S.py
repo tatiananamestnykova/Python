ab2eea9bcfaf80edd40b48588ef14fa004117a1b
unknown <tatiana.namestnykova@gmail.com>
2022-02-06 22:05:27 +0200

    add hw

--- Tasks_S.py
+++ Tasks_S.py
@@ -38,6 +38,7 @@
 #     r = int(input())
 #     s = 3.14 * r**2
 #     print(s)
+
 # Задача_2
 # Напишите программу, которая получает на вход три целых числа,
 # по одному числу в строке, и выводит на консоль в три строки
@@ -51,12 +52,70 @@ import math
 # print(min(m,n,k))
 # print(s-max(m,n,k) - min(m,n,k))
 
-a = int(input())
-if a%10==1 and a%100!= 11:
-    print(a, 'программист')
-elif a%10==2 and a%100!=12 or a%10==3 and a%100!=13 or a%10==4 and a%100!=14:
-    print(a, 'программиста')
-else:
-    print(a, 'программистов')
+# Задача 3
+# a = int(input())
+# if a%10==1 and a%100!= 11:
+#     print(a, 'программист')
+# elif a%10==2 and a%100!=12 or a%10==3 and a%100!=13 or a%10==4 and a%100!=14:
+#     print(a, 'программиста')
+# else:
+#     print(a, 'программистов')
+
+# Задача 4
+# s = str(input())
+# sum1 = int(s[0])+int(s[1])+int(s[2])
+# sum2 = int(s[3])+int(s[4])+int(s[5])
+# if sum1 == sum2:
+#   print('Счастливый')
+# else:
+#   print('Обычный')
+# Задача 5
+# c = int(input())
+# s = 0
+# while c != 0:
+#     s = s + c
+#     c = int(input())
+# print(s)
+
+# Задача 6
+# a = int(input())
+# b = int(input())
+# n = 1
+# m = 2
+# while n<m:
+#   if n % a == 0 and n % b == 0:
+#     n = m
+#   else:
+#     n = n+1
+#     m = n+1
+# print(n)
+
+# Задача 7
+# a = 0
+# while a<=100:
+#   a = int (input())
+#   if a > 100:
+#     break
+#   if a<10:
+#     continue
+#   print(a)
+# Задача 8
+# n = int(input())
+# for i in range(n):
+#   for j in range(n):
+#     print('*', end='')
+#   print()
+# Задача 9
+# a = int(input())
+# b = int(input())
+# s = 0
+# c = 0
+# for i in range(a, b + 1):
+#     if i % 3 == 0:
+#         s = s + i
+#         c = c + 1
+#         i = i + 1
+# print(s / c)
+
 
 
---
+++ lesson.py
@@ -0,0 +1,6 @@
+import sys
+import argparse
+
+print('hello yelochka')
+params = sys.argv[4:7]
+print('params =', params)
